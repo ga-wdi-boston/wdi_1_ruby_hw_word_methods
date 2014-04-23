227a1edc5f93da1a90a281a537f7a3f1f1b3a07d
Ethan Gould <ethan.g.907@gmail.com>
2014-04-23 19:10:13 -0400

    added method to puts word in a givin string into an array, also method to return all instances of words

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,16 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+# puts words within string into an array
+def split_text(file)
+  file.split
+end
+
+# iterates over array to find frequency of word
+def find_unique_words(string)
+  string_no_special = string.gsub(/[\,\.]/, ' ')
+  words = string_no_special.downcase.split
+  words.uniq.sort
+end
+
+# puts find_unique_words(sample_text)
