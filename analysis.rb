d8d80f0f5fb230ced34b7b2eb482c371b53f4812
Lisa Plesko <plesko.l@gmail.com>
2014-04-23 18:13:02 -0400

    Solution for #1

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,23 @@
+require 'pry'
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+# Question 1
+
+def magic_split(text)
+  words = []
+  new_text = text.downcase.gsub(/[.?!,()";:]/," ").gsub(/--/," ") # need --
+  words << new_text.strip.split(" ")
+end
+
+puts magic_split(sample_text)
+
+
+
+
+
+
+
+
+
+binding.pry
