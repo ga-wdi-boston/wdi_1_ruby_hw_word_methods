9f2aed84d3b0f696af055cb198db3ead90b182e7
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-23 18:28:14 -0400

    Create count_words method

--- analysis.rb
+++ analysis.rb
@@ -1,4 +1,5 @@
 # This will give you the contents of the `sample.txt` file as one big string
+
 sample_text = File.read('sample_copy.txt')
 
 def normalize(text)
@@ -8,6 +9,19 @@ end
 def unique(text)
   normalize(text).uniq
 end
+
+def count_words(text, unique: false)
+  if unique == false
+    words = normalize(text)
+    words.index(words.last) + 1
+  else
+    words = unique(text)
+    words.index(words.last) + 1
+  end
+end
+
+
+
 
 
 
