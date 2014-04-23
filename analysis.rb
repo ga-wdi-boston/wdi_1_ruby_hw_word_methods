2ffc19ad25e2d9ace8efc1b2f6f527293d07d14f
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-23 18:11:47 -0400

    Create normalize method

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,8 @@
 # This will give you the contents of the `sample.txt` file as one big string
-sample_text = File.read('sample.txt')
+sample_text = File.read('sample_copy.txt')
+
+def normalize(text)
+  text.downcase.split(/\W+/)
+end
+
+
