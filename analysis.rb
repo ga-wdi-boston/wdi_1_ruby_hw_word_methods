0312efc4fc012e3c6f39714d160b236380e2b74d
George Straehle <gstraehle@gmail.com>
2014-04-24 00:58:36 -0400

    Added sorted hash using deletion and last by asc

--- analysis.rb
+++ analysis.rb
@@ -11,7 +11,7 @@ sample_text = File.read('sample.txt')
 #array1 = sample_text.downcase.gsub(/[^a-z ]/, ' ').split
 
 def strip_text(passage)
-  passage.downcase.gsub(/[^a-z ]/, ' ').split
+  passage.downcase.gsub(/[^a-z ]/, ' ').split#split makes and array
 end
 
 
@@ -19,29 +19,38 @@ end
 # the uniq function can just build off of the previous question...
 
 def strip_text_unique(passage)
-  strip_text(passage).uniq
+  strip_text(passage).uniq#unique
 end
 
 
 #Question3
 # in order to invoke only the unique word count, you must write 'unique: true'
 def word_count(passage, unique: false)
-  !unique ? strip_text(passage).count : strip_text_unique(passage).count
+  !unique ? strip_text(passage).count : strip_text_unique(passage).count# words = rows here
 end
 
 #Question4
 # this initializes a new hash at 0, for each object in the array, 1 gets added
-def make_array_with_count(passage)
-  array_from_excerpt = strip_text(passage)
-  names_and_counts = Hash.new 0
+def make_hash_with_count(passage)
+  array_from_excerpt = strip_text(passage)#input from first question
+  names_and_counts = Hash.new 0 #initializes at 0
   array_from_excerpt.each do |word|
-    names_and_counts[word] += 1
+    names_and_counts[word] += 1#for all subs. occurences, add 1
   end
   names_and_counts # must have the last line return the array!
 end
 
-
+#Question5
+def hash_sort(passage, minlength: 3)
+  hashtest = make_hash_with_count(passage)
+  hashtest.delete_if {|k,v| k.length < minlength}# comparison can act on array
+  hashtest.sort_by {|k,v| v}.last# |key,value| what sorted by (default is asc) and last item
+end
 binding.pry
+
+
+
+
 
 
 
