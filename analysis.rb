42e72eb6fe59b367be614dc75f0faeffb42f7cf3
Lisa Plesko <plesko.l@gmail.com>
2014-04-23 22:36:38 -0400

    Complete questions 2 through 4

--- analysis.rb
+++ analysis.rb
@@ -6,13 +6,34 @@ sample_text = File.read('sample.txt')
 
 def magic_split(text)
   words = []
-  new_text = text.downcase.gsub(/[.?!,()";:]/," ").gsub(/--/," ") # need --
-  words << new_text.strip.split(" ")
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
 end
 
-puts magic_split(sample_text)
 
+# Question 2
 
+def magic_unique(text)
+  words = []
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
+  words = words.uniq
+end
+
+# Question 3
+
+def magic_counting(text,unique: false)
+  words = []
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
+  if unique == true
+    words.uniq.length
+  else
+    words.length
+  end
+end
+
+# Question 4
 
 
 
