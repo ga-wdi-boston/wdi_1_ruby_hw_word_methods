fb0f0d78b452b4872f67bdd7c559a55acf79a8ee
Lisa Plesko <plesko.l@gmail.com>
2014-04-24 08:24:08 -0400

    Add answers 5 and 6

--- analysis.rb
+++ analysis.rb
@@ -35,10 +35,40 @@ end
 
 # Question 4
 
+def magic_occurrence(text)
+  words = []
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
+
+  words_and_lengths = Hash.new(0)
+  words.each { |word| words_and_lengths[word] += 1 }
+  words_and_lengths
+end
+
+# Question 5
+ def magic_common(text,length=3)
+  words = []
+  words_by_length = []
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
+  words.map do |i|
+    if i.length > length
+      words_by_length << i
+    end
+  end
+  words_by_length.group_by {|a| a }.sort_by{|a,b| b.size<=>a.size}.first[0]
+end
+
+# Question 6
+
+def magic_max_length(text)
+  words = []
+  clean_text = text.downcase.gsub(/[.?!,()";:]|(--)/,"")
+  words += clean_text.strip.split(" ")
+
+  longest = words.max_by {|word| word.length }
+  longest
+  # Need to create an array where values == longest.length
+end
 
 
-
-
-
-
-binding.pry
