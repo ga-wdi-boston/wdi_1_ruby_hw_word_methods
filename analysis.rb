2cff129c8f1b2e34f6c34995faf0fec7c188b241
Devin Dreszer <devindreszer@gmail.com>
2014-06-11 18:24:15 -0400

    Add individual_word_counts method
    * Creates a hash of all the words in a text as keys
    * And their word counts as values

--- analysis.rb
+++ analysis.rb
@@ -1,7 +1,9 @@
+require 'pry'
+
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
 
-sample_text2 = "The quick brown fox jumped over the lazy dog."
+sample_text2 = "The the the the quick! brown brown brown fox fox   ;jumped over the lazy lazy dog dog."
 
 # makes all words lower case and scrubs punctuation
 def normalize(text)
@@ -9,8 +11,7 @@ def normalize(text)
 end
 
 def unique_words(text)
-  words = normalize(text).split
-  words.uniq
+  normalize(text).split.uniq
 end
 
 def word_count(text, unique: false)
@@ -21,5 +22,19 @@ def word_count(text, unique: false)
   end
 end
 
+def individual_word_counts(text)
+  word_counts = {}
+  unique_words(text).each do |i|
+    count = 0
+    normalize(text).scan(i) { |x| count += 1 }
+    word_counts[i] = count
+  end
+  word_counts
+end
+
+
+
 puts unique_words(sample_text2)
 puts word_count(sample_text2, unique: true)
+puts individual_word_counts(sample_text)
+
