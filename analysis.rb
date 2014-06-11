afac5f4e14b895e8246e269022a54f21080b8fce
testkitschen <katryn.mcintosh@gmail.com>
2014-06-11 19:08:11 -0400

    Create a method to strip the text clean and push to array.

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,23 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+#Method that splits a string into an array and normalizes all the sample_text
+
+
+def clean_text(sample_text)
+  stripped_text = sample_text.gsub(",","")
+  stripped_text = stripped_text.gsub("?","")
+  stripped_text = stripped_text.gsub(".","")
+  stripped_text = stripped_text.gsub(";","")
+  stripped_text = stripped_text.gsub(":","")
+  stripped_text = stripped_text.gsub("(","")
+  stripped_text = stripped_text.gsub(")","")
+  stripped_text = stripped_text.gsub('"',"")
+  stripped_text = stripped_text.gsub("'","")
+  stripped_text = stripped_text.gsub("!","")
+  stripped_text = stripped_text.gsub("-","")
+  stripped_text.split.map(&:downcase)
+end
+
+
+puts clean_text(sample_text)
