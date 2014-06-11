6dae0bebcaa087e297eaf655ef081fec701b3ff5
Devin Dreszer <devindreszer@gmail.com>
2014-06-11 17:43:43 -0400

    Create normalize method
    * Makes text downcase without punctuation

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,10 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+# makes all words lower case and scrubs punctuation
+def normalize(text)
+  text.downcase.gsub(/[^a-z ]/, '')
+end
+
+
+puts normalize(sample_text)
