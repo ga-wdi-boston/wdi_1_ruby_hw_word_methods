7689c1aae0e2e219225c7c26f7b029eeab532ef1
csb324 <csb324@gmail.com>
2014-06-11 19:30:20 -0400

    Refactor step 1 -- remove loop

--- analysis.rb
+++ analysis.rb
@@ -2,7 +2,6 @@ require 'pry'
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
 
-
 # 1. Method that splits a string into an array of normalized words.
 # "Normalized" here means that the words should be all lowercase
 # and scrubbed of any punctuation such as periods or parentheses.
@@ -10,10 +9,7 @@ sample_text = File.read('sample.txt')
 def normalize(text)
   normalized = text.gsub(" '"," ").gsub("' "," ")
   normalized.delete! "." "," "(" ")" ";" "!" ":" "?" "\""
-
-  normalized.split.map do |word|
-    word = word.downcase
-  end
+  normalized.downcase.split
 end
 
 # 2. Method that takes a string and returns an array of unique* words
@@ -36,21 +32,18 @@ end
 # 4. Method that takes a string and returns a hash where the keys are normalized words, and the values are integers representing how many times that word occurs in the string.
 
 def each_word_count(text)
-
   word_counts = {}
   unique_words(text).each do |word|
     word_counts[word] = 0
   end
-
   normalize(text).each do |word|
     word_counts[word] += 1
   end
-
   word_counts
-
 end
 
-# 5. Method that finds the most common word in a string that is longer than a particular "length threshold". The threshold should be optional, and default to 3 characters (i.e. words of 3 characters or less will not be considered). It's up to you how to handle multiple words being "tied" for most common.
+# 5. Find the most common word in a string that is longer than some threshold
+# The threshold should be optional, and default to 3 characters
 
 def most_common(text, too_short: 3)
   long_words = each_word_count(text).select do |word, count|
@@ -58,12 +51,13 @@ def most_common(text, too_short: 3)
   end
 
   highest_frequency = long_words.values.max
-  longest_words = long_words.select do |word, count|
+  long_words.select! do |word, count|
     count == highest_frequency
   end
 end
 
-binding.pry
-
 # * Write a method that finds the length of the longest word in a string,
 # returns an array of all unique words in the string that are that length.
+
+def longest_words
+end
