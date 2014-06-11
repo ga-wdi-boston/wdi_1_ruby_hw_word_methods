2ed2fba5abea8026731035a74a68134fbc2284ad
Devin Dreszer <devindreszer@gmail.com>
2014-06-11 17:49:43 -0400

    Add remove_duplicate_words method
    * Takes in sample text
    * Calls normalize and converts string to array
    * Removes duplicate words in the array

--- analysis.rb
+++ analysis.rb
@@ -1,10 +1,17 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
 
+sample_text2 = "The quick brown fox jumped over the lazy dog."
+
 # makes all words lower case and scrubs punctuation
 def normalize(text)
   text.downcase.gsub(/[^a-z ]/, '')
 end
 
+def remove_duplicate_words(text)
+  words = normalize(text).split
+  words.uniq
+end
 
-puts normalize(sample_text)
+puts remove_duplicate_words(sample_text2)
+
