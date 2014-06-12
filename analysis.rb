89d47f4c782eda833b5d50b20efa20f240a48fd9
Jake Austin <jakeaust07@gmail.com>
2014-06-11 22:03:40 -0400

    count occurances of each unique word in original string, abstracted to unique_words_count method

--- analysis.rb
+++ analysis.rb
@@ -7,29 +7,46 @@ sample_text = File.read('sample.txt')
 #   - split each word into chars
 #   - delete non-alpha chars
 #   - join chars back into words
-def normalise_text(text)
+def normalize_text(text)
   text.split.map { |word| word.chars.
     delete_if{ |c| /[[:alpha:]]/.match(c).nil? }.join.downcase }
 end
 
-normalised = normalise_text(sample_text)
 
 # Find unique words in text
-#   - normalise text
+#   - normalisz text
 #   - create set from array of all words
 def unique_words(text)
-  all_words = normalise_text(text)
+  all_words = normalize_text(text)
   all_words & all_words
 end
 
-words_set = unique_words(sample_text)
 
+# Find number of words in text
+#   - option to find number of unique words
+#   - use count in conjunction with either normalise_words or unique_words
 def how_many_words(text, unique: false)
-  unique ?  unique_words(text).count : normalise_text(text).count
+  unique ?  unique_words(text).count : normalize_text(text).count
 end
 
-all_words = how_many_words(sample_text)
-unique_words = how_many_words(sample_text, unique: true)
+# tests
+# all_words = how_many_words(sample_text)
+# unique_words = how_many_words(sample_text, unique: true)
 
+# Create hash of unique word => number of occurances
+def unique_words_counts(text)
+  words_set = unique_words(text)
+  normalized = normalize_text(text)
+  counts = words_set.map{ |word| normalized.collect{ |string| string if
+    string == word}.compact.count}
+  Hash[words_set.zip(counts)]
+end
+
+worsd_count_hash = unique_words_counts(sample_text)
+
+# Find most common word that is longer than an optional threshhold (default 3)
+
+# Challenge: find length of longest word, return array of all unique words
+#            of "longest" length
 
 binding pry
