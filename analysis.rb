068064350539468f39eee7f99407204d416941c7
Jodi Detch <jodidetch@gmail.com>
2014-06-11 21:04:25 -0400

    Added word_count method, but it does not ignore punct & cap diffs

--- analysis.rb
+++ analysis.rb
@@ -8,4 +8,17 @@ def normalize
   str.downcase.gsub /\W+/, ' '
 end
 
+# How can I simplify this?
+# How can I reference the normalize method in the word_count method?
+def word_count
+  words = []
+  sample_text = File.read('sample.txt')
+  words = sample_text.split(/[^a-zA-Z]/)
+  freqs = Hash.new(0)
+  words.each { |word| freqs[word] += 1 }
+  freqs = freqs.sort_by {|x,y| y }
+  freqs.reverse!
+  freqs.each {|word, freq| puts word+' '+freq.to_s}
+end
+
 binding.pry
