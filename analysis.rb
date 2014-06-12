fa63ba9d1967fe112cd87ee61db53c321fd13b1a
Julie <juliesof@gmail.com>
2014-06-11 20:13:25 -0400

    Normalized the text and set variable with text as array of normalized words

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,5 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+normalized_text = sample_text.downcase.gsub(/[^a-z0-9\s]/i, '')
+split_word_text = lwrcase_text.split #tested & IS array of indiv words
+
