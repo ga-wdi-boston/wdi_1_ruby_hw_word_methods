878d986f83d022943af40e00de3d1582f1c0bf8d
Jake Austin <jakeaust07@gmail.com>
2014-06-11 20:36:41 -0400

    split text into word array, normalize words

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,15 @@
+require 'pry'
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+# Normalize all words in text
+# Split text into array of words
+#   - split each word into chars
+#   - delete non-alpha chars
+#   - join chars back into words
+
+normalized_words = sample_text.split.map { |word|
+  word.chars.delete_if { |c| /[[:alpha:]]/.match(c).nil? }.join.downcase }
+
+
+binding pry
