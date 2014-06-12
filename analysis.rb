da50d31f073864bc8e73eedcf5375c58dd39390e
Max Kohl <maxkohl810@gmail.com>
2014-06-11 21:17:05 -0400

    create first two methods
    	* alice_normalize will take a string and return an array
    	of words included in the string. these words will be
    	lowercase and scrubbed of punctuation.
    
    	* alice_unique_words will take a string and run the
    	alice_normalize method on it. Then, it will produce an
    	array of all unique elements in the outputted array.

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,22 @@
+require 'pry'
+
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+## QUESTION 1
+
+def alice_normalize(text)
+  text.downcase.gsub(/[(,?!"'\:;.)]/, ' ').gsub('-', ' ').split(' ')
+end
+
+puts alice_normalize(sample_text)
+
+## QUESTION 2
+
+def alice_unique_words(text)
+  alice_normalize(text).uniq
+end
+
+puts alice_unique_words(sample_text)
+
+binding.pry
