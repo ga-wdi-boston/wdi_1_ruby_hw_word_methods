2abd3e0180d79d0e0059d34f7b5ce3b04068b05e
sarahchristine913 <s.mele13@gmail.com>
2014-06-12 09:31:23 -0400

    Defined first two method
    * Defined normalize method
    * Defined no duplicate method to build on normalize and show only unique words

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,26 @@
-# This will give you the contents of the `sample.txt` file as one big string
+require ='pry'
+
+# This will give you the contents of the `sample.txt` file as one big strin
+
+# method to split string into an array of strings
+# makes all array strings downcase and without punct
+# returns altered array
+
+def normalize(text_string)
+  text_string.downcase.gsub(/[^a-z0-9\s]/i, '').split
+end
+
+def no_duplicates(text_string)
+  normalize(text_string).uniq
+end
+
 sample_text = File.read('sample.txt')
+
+puts no_duplicates(sample_text)
+# create method that accepts normalized array
+# use .uniq to show unique words
+# must also set up variable for altered string
+
+
+#
+
