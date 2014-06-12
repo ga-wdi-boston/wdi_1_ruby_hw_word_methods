3c766adf6c056be8c8b38ed5bed6b4fd0d599127
Jodi Detch <jodidetch@gmail.com>
2014-06-11 20:09:12 -0400

    Added method "normalize" that split sample_text str into array of words

--- analysis.rb
+++ analysis.rb
@@ -1,4 +1,9 @@
 # This will give you the contents of the `sample.txt` file as one big string
-sample_text = File.read('sample.txt')
+require 'pry'
 
-puts sample_text.split
+def normalize
+  sample_text = File.read('sample.txt')
+  sample_text.split
+end
+
+binding.pry
