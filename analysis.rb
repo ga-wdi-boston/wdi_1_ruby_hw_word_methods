c3d3c28707d1aceafb7d8b0836820b77854e1b13
krk324 <krk32488@gmail.com>
2014-06-11 20:26:13 -0400

    Add count method and count_word_occurrence method.

--- analysis.rb
+++ analysis.rb
@@ -25,12 +25,26 @@ sample_text = File.read('sample.txt')
 
 def normalize(text)
   # lowercase and remove punctuation then push it into a string.
-  # puctuations = [".", "?", "/", "(", ")", ";", ":"]
-  text.downcase!.gsub(".",'').gsub("?",'').gsub("/",'').gsub("(",'').gsub(")",'').gsub(";",'').gsub(":",'').gsub(",",'').gsub("\"",'').gsub("!",'').gsub("-",'').gsub("'",'').split
+  # puctuations = [".", "?", "/", "(", ")", ";", ":"] #gsub is too long. thinking of way to loop.
+  text.downcase.gsub(".",'').gsub("?",'').gsub("/",'').gsub("(",'').gsub(")",'').gsub(";",'').gsub(":",'').gsub(",",'').gsub("\"",'').gsub("!",'').gsub("-",'').gsub("'",'').split
 end
 
-# create array ony with unique words
 def unique_words(text)
   normalize(text).uniq
 end
 
+def count_words(text, type: 'total')
+  type == 'unique' ? unique_words(text).length : normalize(text).length
+end
+
+def count_words_occurence(text)
+  normalize(text).each_with_object(Hash.new(0)) { |text,counts| counts[text] += 1 }
+end
+
+
+
+
+puts count_words(sample_text)
+puts count_words(sample_text, type: 'unique')
+
+puts count_words_occurence(sample_text)
