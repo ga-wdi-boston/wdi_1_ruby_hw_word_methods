4097ebc9d22bf1e95a904694a91c88298530f1f8
afeinber <alexandernfeinberg@gmail.com>
2014-06-11 20:14:01 -0400

    Made a normalize method for strings

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,8 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+def normalize(str)
+  str.gsub(/[^a-zA-Z\s]/, '').gsub(/\n/, ' ').downcase.split(' ')
+end
+
+puts normalize(sample_text)
