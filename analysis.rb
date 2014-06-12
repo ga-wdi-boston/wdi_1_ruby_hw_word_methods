fbee03bc9c794de3f3ada6bd05e336481d55806f
krk324 <krk32488@gmail.com>
2014-06-11 20:51:01 -0400

    Add method largest_hash_value. Add test puts statements.

--- analysis.rb
+++ analysis.rb
@@ -19,8 +19,25 @@ def count_words_occurence(text)
   normalize(text).each_with_object(Hash.new(0)) { |text,counts| counts[text] += 1 }
 end
 
+def most_common_words(text)
+  count_words_occurence(text)
+end
+
+def largest_hash_value(text)
+  count_words_occurence(text).max_by{|k,v| v}
+end
+
+puts normalize(sample_text).inspect
+
+puts unique_words(sample_text).inspect
 
 puts count_words(sample_text)
 puts count_words(sample_text, type: 'unique')
 
 puts count_words_occurence(sample_text)
+
+puts most_common_words(sample_text)
+
+print largest_hash_value(sample_text)
+puts
+
