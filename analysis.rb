02edb7c0b0f036f1ed14624d1be1fb23bf1fbd40
Jake Austin <jakeaust07@gmail.com>
2014-06-11 22:15:22 -0400

    use unique words => counts hash to calculate most common word (over optional minimum length)

--- analysis.rb
+++ analysis.rb
@@ -1,3 +1,8 @@
+# Jake Austin
+# GA WDI Boston June
+# hw3
+# Jun 11, 2014
+
 require 'pry'
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
@@ -6,7 +11,7 @@ sample_text = File.read('sample.txt')
 # Split text into array of words
 #   - split each word into chars
 #   - delete non-alpha chars
-#   - join chars back into words
+#   - join chars back into words, downcase
 def normalize_text(text)
   text.split.map { |word| word.chars.
     delete_if{ |c| /[[:alpha:]]/.match(c).nil? }.join.downcase }
@@ -34,19 +39,28 @@ end
 # unique_words = how_many_words(sample_text, unique: true)
 
 # Create hash of unique word => number of occurances
+#    - Find unique words
+#    - go through all words, aggregate counts of unique words
 def unique_words_counts(text)
   words_set = unique_words(text)
-  normalized = normalize_text(text)
-  counts = words_set.map{ |word| normalized.collect{ |string| string if
+  all_words = normalize_text(text)
+  counts = words_set.map{ |word| all_words.collect{ |string| string if
     string == word}.compact.count}
   Hash[words_set.zip(counts)]
 end
 
-worsd_count_hash = unique_words_counts(sample_text)
-
 # Find most common word that is longer than an optional threshhold (default 3)
+#    - use hash to find most occurances
+#    - tie defaults to whichever word appears first
+def words_mode(text, min_length: 3)
+  words_and_counts = unique_words_counts(text)
+  words_and_counts.delete_if{ |key, val| key.length < min_length }
+  words_and_counts.key(words_and_counts.values.max)
+end
 
 # Challenge: find length of longest word, return array of all unique words
 #            of "longest" length
+
+
 
 binding pry
