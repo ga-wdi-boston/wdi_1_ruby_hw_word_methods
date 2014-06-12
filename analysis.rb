babe655713f1dd478dd97138bf2de1ab4d781a5b
Emma Gardner <emmagardner@outlook.com>
2014-06-12 07:41:12 -0400

    create method to find most common word in string
    * make a hash using word_hash_count
    
    * select items in that hash with words greater than length 3
    
    * find the word with the highest count

--- analysis.rb
+++ analysis.rb
@@ -8,38 +8,44 @@ def splits(string)
   normalized_array = string.downcase.scan(/\w+/)
 end
 
+puts splits(sample_text)
+puts
+
 # Method that takes a string and returns an array of all unique words in it (ignoring punctuation and capitalization)
 def unique(string)
   unique_array = splits(string).uniq
 end
 
+puts unique(sample_text)
+puts
 # Method that counts how many words are in a string- option to count unique words instead of total words
 def counts(string, uniq_op = false)
   str_count = string.split.count
   uniq_op == true ? unique(string).count : str_count
 end
 
-
-
+puts counts(sample_text)
+puts
 # Method that takes a string and returns a hash where the keys are normalized words, and the values are ints representing how many times that word occurs in the string
 def word_hash_count(string)
-  # take a string and turn into an array
   str_arr = splits(string)
   count_array = str_arr.group_by { |i| i }.map { |k,v| [k,v.count] }
   count_hash = Hash[*count_array.flatten]
-
-
-  # keys are normalized words
-  # values are integers representing how many times that word occurs in the string- USE counts method you created above
 end
-binding.pry
 
+puts word_hash_count(sample_text)
+puts
 
 # Method that finds the most common word in a string that is longer than a particular "length threshold". The threshold should be optional, and default to 3 chars. How to handle multiple words being 'tied' for most common.
-def most_common
-  # find the most common word in a string that is longer than a specified "length threshold"- USE counts method you created above.
-  # threshold is optional and the default should be 3 chars
-  # decide how to handle multiple words being tied for most common.
+def most_common(string, leng_thresh = 3)
+  hsh = word_hash_count(string)
+  word_len_hsh = hsh.select { |k,v| k.length > 3 }
+  common_word = word_len_hsh.max_by { |k,v| v }[0]
 end
+
+puts most_common(sample_text)
+puts
+
+binding.pry
 
 # COMBINE METHODS WHENEVER POSSIBLE
