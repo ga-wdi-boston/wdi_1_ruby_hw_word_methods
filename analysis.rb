4bc6ac39178eae3bb15d1af9de6c93d9a1390098
dmcgann1 <dmcgann1@gmail.com>
2014-06-11 23:21:59 -0400

    Method 2 almost complete - not yet working

--- analysis.rb
+++ analysis.rb
@@ -1,7 +1,7 @@
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
 
-test = "THis, is a test!"
+test = "THis, is a test! this is is is"
 
 #Method 1 to normalize a string of text
 #Splits text into an array of characters
@@ -13,8 +13,16 @@ end
 
 puts normalize(test)
 
+#Method 2 to map only unique words to a new array - not yet working
+def unique(text)
+  normalized_text = normalize(text)
+  unique_array = []
+  normalized_text.map! {|word| word unless unique_array.include?(word)}
+end
+
+puts unique(test)
+
 
 #Method 2
 #Call method 1 to remove all punctuation and capitalisation issues
 #Map all words to new array only if the array does not already contain that words
-
