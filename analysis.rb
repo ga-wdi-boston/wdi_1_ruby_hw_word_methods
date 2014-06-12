4b85d8f38c6d56884cfd20d59e8141c0e3617c0e
EricRho <eric.rho.87@gmail.com>
2014-06-11 22:39:48 -0400

    Create method to downcase all characters and remove grammatical characters (still need spaces and put into array)

--- analysis.rb
+++ analysis.rb
@@ -2,9 +2,16 @@
 sample_text = File.read('sample.txt')
 
 # split and normalise text
+def split_normalise(text)
+  text.downcase.gsub(/[^a-z]/, '').split
+end
+
+puts split_normalise(sample_text)
 
 # method to return unique words
 
 # method for text count
 
-# method
+# method for frequency of words in a hash
+
+# method to find most common words in a string longer than a threshold
