41ed021c46798e9a7d877c10005010d78060c0a2
Max Kohl <maxkohl810@gmail.com>
2014-06-11 22:40:56 -0400

    define alice_count_words_optional
    	* this method will take in the sample_text, and then will
    	ask the user if they would like to count all words or count
    	only unique words.

--- analysis.rb
+++ analysis.rb
@@ -4,19 +4,38 @@ require 'pry'
 sample_text = File.read('sample.txt')
 
 ## QUESTION 1
+# this returns an array of normalized words found in the first chapter of AIW.
 
 def alice_normalize(text)
   text.downcase.gsub(/[(,?!"'\:;.)]/, ' ').gsub('-', ' ').split(' ')
 end
 
-puts alice_normalize(sample_text)
 
 ## QUESTION 2
+# take the returned array from alice_normalize and return a new array of unique words.
 
 def alice_unique_words(text)
   alice_normalize(text).uniq
 end
 
-puts alice_unique_words(sample_text)
+puts alice_normalize(sample_text).count
+puts alice_unique_words(sample_text).count
+
+## QUESTION 3
+# count the number of words in AIW: Chapter 1, and provide the user an option
+# to count only unique words.
+
+puts "Would you like to count only unique words? Please say yes or no."
+request_unique = gets.chomp
+
+def alice_count_words_unique_optional(text, unique: 'no')
+  if unique == "no"
+    alice_normalize(text).count
+  else
+    alice_unique_words(text).count
+  end
+end
+
+puts alice_count_words_unique_optional(sample_text, unique: request_unique)
 
 binding.pry
