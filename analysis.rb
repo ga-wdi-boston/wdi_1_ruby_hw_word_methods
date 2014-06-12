445dc7b438a63a334efb2611546c638ab745d9f5
brimatteng <btenggren20@gmail.com>
2014-06-11 20:36:13 -0400

    finishes first three methods

--- analysis.rb
+++ analysis.rb
@@ -1,10 +1,11 @@
+require 'pry'
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
 
 # method for normalizing text
 # takes whole sample text,
 # returns array of words with no punctuation or spaces
-def make_simple (whole_text)
+def make_simple(whole_text)
   whole_text.gsub(/[.!?"')(:;,-]/, '').downcase.split
   # return array of strings
 end
@@ -13,19 +14,20 @@ end
 # takes a String
 # returns an array of all of it's unique words
 def find_unique (whole_text)
+  make_simple(whole_text).uniq
   # return array of strings
 end
 
 # method for counting how many words are in a string
 # takes *option* to count unique words instead of total words returns count
 # returns an int for the count
-def get_wordcount (whole_text, unique: false)
-  whole_text.scan(/\w+/){|w| }
+def get_wordcount (whole_text, countunique: false)
+  countunique ? find_unique(whole_text).length : make_simple(whole_text).length
   # return an int
 end
 
 # method for counting number of occurences of a word in a string
-# takes a string and
+# takes a string
 # returns a hash where the keys are normalized words and the values are integers representing the number of occurences.
 def get_occurence_count (whole_text)
   # return a hash (keys are strings, vals are ints)
@@ -38,4 +40,5 @@ def most_common (letter_length: 3)
   # return string
 end
 
-puts make_simple(sample_text)
+puts get_wordcount(sample_text, countunique: true)
+
