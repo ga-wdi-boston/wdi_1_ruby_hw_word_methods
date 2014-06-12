d05759b52ebc2019400d9e1649e474de1d7827c5
Jake Austin <jakeaust07@gmail.com>
2014-06-11 20:45:20 -0400

    create set to find unique words, abstracted into unique_words method

--- analysis.rb
+++ analysis.rb
@@ -7,12 +7,21 @@ sample_text = File.read('sample.txt')
 #   - split each word into chars
 #   - delete non-alpha chars
 #   - join chars back into words
-
 def normalise_text(text)
   text.split.map { |word| word.chars.
     delete_if{ |c| /[[:alpha:]]/.match(c).nil? }.join.downcase }
 end
 
 normalised = normalise_text(sample_text)
+
+# Find unique words in text
+#   - normalise text
+#   - create set from array of all words
+def unique_words(text)
+  all_words = normalise_text(text)
+  all_words & all_words
+end
+
+words_set = unique_words(sample_text)
 
 binding pry
