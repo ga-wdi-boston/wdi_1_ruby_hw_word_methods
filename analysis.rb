812ba357d1b0b234eecb121750b55005b164f250
nnovak13 <nnovak@fordham.edu>
2014-06-12 07:26:39 -0400

    Finalize method descriptions

--- analysis.rb
+++ analysis.rb
@@ -2,60 +2,32 @@
 
 sample_text = File.read('sample.txt')
 
-
+##reates normalise method: splits string to array, strips punctuation
 def normalize(text)
   text.downcase.gsub("'","").gsub(/[^a-z ]/, ' ').split
 end
 
-#puts normalize(sample_text)
-
-
+#creates new array with normalized, unique words
 def unique_words(text)
   normalize(text).uniq
 end
 
-#puts unique_words(sample_text)
-
+#creates word count & condition for unique v. total words
 def word_count(text, unique: false)
   unique ? unique_words(text).count : normalize(text).count
 end
-#puts word_count(sample_text, unique: true)
 
+#creates word frequency hash, with k as words and v as frequency
 def word_frequency(text)
   norm_array = normalize(text).to_a
   freq = { }
   norm_array.each_with_object(Hash.new(0)){|key,hash| hash[key] += 1}
 end
-#puts word_frequency(sample_text)
 
+#creates threshold for word frequency,
+#setting lower bound on frequency and option for length threshold
 def threshold(text, threshold: false)
  thresh = (word_frequency(text).select { |k, v| v > 4})
-  threshold ? thresh.reject {|k,v| k.length < 6} : thresh.reject {|k,v| k.length < 4}
+ threshold ? thresh.reject {|k,v| k.length < 6} : thresh.reject {|k,v| k.length < 4}
 end
-puts threshold(sample_text, threshold: true)
-
-  #threshold_array = (normalize(text).to_a
-  #thresh = { }
-  #threshold_array.each_with_object(Hash.new(0)){|key,hash| hash[key.length(key)] += 1}
-
-  #thresh = (word_frequency(text).select { |k, v| k.length >3, v > 15})
-  #thresh = (word_frequency(text).select { |k, v| puts k if v ==hash.values.max})
-  #thresh = (word_frequency(text).select { |k, v| puts k if v ==hash.values.max && hash.values.length >3})
-  #if
-    #normalize(text).length > 3
-
-  #threshold ? v.length >3: normalize(text).count
-  #threshold ? word_frequency(text).length >3 : normalize(text).count
-#end
-
-
-#.inspect
-
-  #Hash.new { |hash, key| hash[key] =  [unique_words(sample_text)]unique_words(sample_text).count }
-  #normalize(text).count
-  #frequency = {normalize(text).to_s => normalize(text).count}
-  #normalize(text) = normalize(text).count #key (need to separate)
-  #values = .count
-
-#puts unique_words(sample_text).split
-
+#puts threshold(sample_text, threshold: true)
