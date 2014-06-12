30020b59c027cc1504ba21533b86c9f13fec5067
sarahchristine913 <s.mele13@gmail.com>
2014-06-12 09:38:32 -0400

    Added Text Length method w/ option
    * Defined new method text_length
    * Added key option for whether the count was for all words or unique words

--- analysis.rb
+++ analysis.rb
@@ -5,6 +5,7 @@ require ='pry'
 # method to split string into an array of strings
 # makes all array strings downcase and without punct
 # returns altered array
+sample_text = File.read('sample.txt')
 
 def normalize(text_string)
   text_string.downcase.gsub(/[^a-z0-9\s]/i, '').split
@@ -14,7 +15,13 @@ def no_duplicates(text_string)
   normalize(text_string).uniq
 end
 
-sample_text = File.read('sample.txt')
+def text_length(text_string, unique: false)
+  if unique: == false
+    text_string.length
+  else
+    no_duplicates(text_string).length
+  end
+end
 
 puts no_duplicates(sample_text)
 # create method that accepts normalized array
